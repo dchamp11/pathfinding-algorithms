bbd9f6f31356695cee7fe63e0c28ac3d7e66ee6f
David Champion <dchampion11@gmail.com>
2024-05-13 13:15:06 -0500

    Create maze grid

--- src/MazeGrid.jsx
+++ src/MazeGrid.jsx
@@ -10,7 +10,7 @@ export default function MazeGrid() {
     ["wall", "wall", "wall", "wall"],
   ];
 
-  const [maze, setMaze] = useState(initialMaze)
+  const [maze, setMaze] = useState(initialMaze);
 
   function generateMaze(height, width) {
     let matrix = [];
@@ -18,21 +18,28 @@ export default function MazeGrid() {
     for (let i = 0; i < height; i++) {
       let row = [];
       for (let j = 0; j < width; j++) {
-        let cell = Math.random()
-        row.push('wall')
+        let cell = Math.random();
+        row.push("wall");
       }
       matrix.push(row);
     }
     console.log(matrix);
-    
-    const dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]];
 
-    function isCellvalid(x,y) {
-      return y >= 0 && x >= 0 && x < width && y < height && matrix[y][x]        === "wall"
+    const dirs = [
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0],
+    ];
+
+    function isCellvalid(x, y) {
+      return (
+        y >= 0 && x >= 0 && x < width && y < height && matrix[y][x] === "wall"
+      );
     }
 
-    function carvePath(x,y) {
-      matrix[y][x] = 'path';
+    function carvePath(x, y) {
+      matrix[y][x] = "path";
 
       const directions = dirs.sort(() => Math.random() - 0.5);
 
@@ -40,22 +47,23 @@ export default function MazeGrid() {
         const nx = x + dx * 2;
         const ny = y + dy * 2;
         if (isCellvalid(nx, ny)) {
-          matrix[y + dy][x + dx] = 'path';
-          carvePath(nx, ny)
+          matrix[y + dy][x + dx] = "path";
+          carvePath(nx, ny);
         }
       }
     }
-    carvePath(1,1);
+    carvePath(1, 1);
     matrix[1][0] = "start";
-    matrix[height -2][width - 1] = "end";
+    matrix[height - 2][width - 1] = "end";
     setMaze(matrix);
   }
 
   return (
     <div className="maze-grid">
-      <button className={"maze-button"} onClick={() => generateMaze(10, 
-      10)}>Refresh Maze</button>
-      <div className="maze">     
+      <button className={"maze-button"} onClick={() => generateMaze(10, 10)}>
+        Refresh Maze
+      </button>
+      <div className="maze">
         {maze.map((row, rowIndex) => (
           <div className="row">
             {row.map((cell, cellIndex) => (
@@ -64,6 +72,6 @@ export default function MazeGrid() {
           </div>
         ))}
       </div>
-    </div>  
+    </div>
   );
 }
